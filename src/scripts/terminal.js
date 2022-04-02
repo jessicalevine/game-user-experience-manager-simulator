548f7f2242ba4fef366d242366addd575b98f863
Jessica Levine <jessica.s.levine@gmail.com>
2022-04-02 12:50:40 -0400

    Add Login Success + Refactor authflow & focusbox

--- src/scripts/terminal.js
+++ src/scripts/terminal.js
@@ -1,9 +1,18 @@
-window.focusbox = function() {
-  var v = document.getElementById("textbox-username").value;
+window.focusbox = function(boxvar) {
+  console.log("func");
+  console.log(boxvar);
+  var v = document.getElementById("textbox-" + boxvar).value;
 
   if(v === "■") {
-    document.getElementById("textbox-username").value = "";
+    document.getElementById("textbox-" + boxvar).value = "";
   }
 
-  document.getElementById("textbox-username").focus()
+  document.getElementById("textbox-" + boxvar).focus();
 }
+
+Macro.add('focusbox', {
+  handler: function() {
+    console.log(this);
+    window.focusbox(this.args[0]);
+  }
+});
